b1bbae1c36899a68fe81c4eb66c76bdffe43af00
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-06-03 17:22:07 +0900

    [change] session周りの通信部分の実装を行いました。

--- lib/data/retrofit/service.dart
+++ lib/data/retrofit/service.dart
@@ -1,11 +1,12 @@
 import 'package:dio/dio.dart';
+import 'package:poipla_app/models/entities/session/session.dart';
 import 'package:poipla_app/models/services/token_service.dart';
 import 'package:poipla_app/models/entities/user/user.dart';
 import 'package:retrofit/http.dart';
 import 'package:retrofit/retrofit.dart';
 part 'service.g.dart';
 
-@RestApi(baseUrl: "https://poipla.yumekiti.net/api")
+@RestApi(baseUrl: 'https://poipla.yumekiti.net/api')
 abstract class PoiplaApiService {
   factory PoiplaApiService(Dio dio, {String baseUrl}) = _PoiplaApiService;
 
@@ -14,6 +15,12 @@ abstract class PoiplaApiService {
 
   @POST('/register')
   Future<Map<String, dynamic>> register();
+
+  @POST('/dust-boxes/{boxId}/sessions')
+  Future<Session> createSession(@Path('boxId') String boxId);
+
+  @PUT('/dust-boxes/{boxId}/sessions/{sessionId}')
+  Future<void> completeSession(@Path('boxId') String boxId, @Path('sessionId') String sessionId);
 }
 
 PoiplaApiService create(TokenService service,
---
+++ lib/models/repositories/session_repository.dart
@@ -0,0 +1,12 @@
+
+import 'package:poipla_app/models/entities/session/session.dart';
+
+class SessionRepository {
+  Future<Session> create({required int dustBoxId}) {
+    throw UnimplementedError();
+  }
+
+  Future<void> complete(Session session) {
+    throw UnimplementedError();
+  }
+}
