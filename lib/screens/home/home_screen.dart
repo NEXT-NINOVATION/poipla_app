18fcb1b17d81dc471e4057fd594f6c3ed7c319ea
Ryota-nino <ryota.swimmer@icloud.com>
2022-06-09 16:31:10 +0900

    [add] ホーム画面を別ファイルに切り離した

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import 'package:poipla_app/constants.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:poipla_app/screens/change_costume/change_costume_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: FractionalOffset.topLeft,
+          end: FractionalOffset.bottomRight,
+          colors: kBackgroundColors,
+          stops: [
+            0.0,
+            1.0,
+          ],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: Text(
+            widget.title,
+            style: const TextStyle(
+              color: kFontColor,
+            ),
+          ),
+          backgroundColor: kAppBarColor,
+          elevation: 0.0,
+        ),
+        body: Stack(
+          children: [
+            Center(
+              child: SizedBox(
+                // Fix
+                height: 200,
+                child: Stack(
+                  children: [
+                    Container(
+                      width: 200,
+                      height: 200,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color(0xFFD9D9D9),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Container(
+                          width: 20,
+                          height: 20,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Container(
+                          width: 10,
+                          height: 10,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    margin: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Column(
+                          children: [
+                            Container(
+                              width: 48,
+                              height: 48,
+                              margin: const EdgeInsets.only(bottom: 3.0),
+                              child: SvgPicture.asset(
+                                "assets/svg/present.svg",
+                                color: kIconColor,
+                              ),
+                            ),
+                            const Text(
+                              "プレゼント",
+                              style: TextStyle(
+                                color: kFontColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 8.0),
+                        Stack(
+                          alignment: Alignment.bottomCenter,
+                          children: [
+                            Container(
+                              width: 48,
+                              height: 48,
+                              decoration: BoxDecoration(
+                                  color: const Color(0xFFD9D9D9),
+                                  borderRadius: BorderRadius.circular(4.0)),
+                            ),
+                            const Text(
+                              "QR",
+                              style: TextStyle(color: kFontColor),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 80),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Container(
+                        width: 90,
+                        height: 90,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Color(0xFFD9D9D9),
+                        ),
+                      ),
+                      Container(
+                        width: 120,
+                        height: 120,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Color(0xFFD9D9D9),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ChangeCostumeScreen(),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          width: 90,
+                          height: 90,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFFD9D9D9),
+                          ),
+                          child: const Center(
+                            child: Text("きせかえ"),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
