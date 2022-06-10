b5ca976a9db17b1fc7b3b3b85c91de7494003663
Ryota-nino <ryota.swimmer@icloud.com>
2022-06-10 14:24:24 +0900

    [add]constants.dartを追加

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// カラー変数
+const kAppBarColor = Color(0xFFC4C4C4);
+const kFontColor = Color(0xFF403F4C);
+const kFontColorOrange = Color(0xFFFFA63E);
+const kBackgroundColors = [Color(0xFFB6E9EB), Color(0xFF207CAF)];
+const kIconColor = Color(0xFF4B4B4B);
+// const kBackgroundColorStart = Color(0xFFB6E9EB);
+// const kBackgroundColorEnd = Color(0xFF207CAF);
