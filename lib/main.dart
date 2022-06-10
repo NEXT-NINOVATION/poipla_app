117179e1747c721ad0475c00ef5362df7f14bd39
Ryota-nino <ryota.swimmer@icloud.com>
2022-06-10 14:21:41 +0900

    [change]ホーム画面を別ファイルに切り離し

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:poipla_app/constants.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:poipla_app/screens/change_costume/change_costume_screen.dart';
+import 'package:poipla_app/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,75 +21,6 @@ class MyApp extends StatelessWidget {
           fontFamily: "LightNovelPOPv2",
         ),
         home: const HomeScreen(title: 'にのさんのおうち'),
-      ),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          widget.title,
-          style: TextStyle(
-            color: kFontColor,
-          ),
-        ),
-        backgroundColor: kAppBarColor,
-        elevation: 0.0,
-      ),
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: FractionalOffset.topLeft,
-            end: FractionalOffset.bottomRight,
-            colors: kBackgroundColors,
-            stops: [
-              0.0,
-              1.0,
-            ],
-          ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ],
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
