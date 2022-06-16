7d327956185f2ecb65b2468c391c79166d4c6559
Ryota-nino <ryota.swimmer@icloud.com>
2022-06-16 18:07:39 +0900

    [add]カラー変数追加

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 
 // カラー変数
 const kAppBarColor = Color(0xFFFFA63E);
-const kFontColor = Color(0xFFFFFFFF);
+const kAppBarFontColor = Color(0xFFFFFFFF);
+const kFontColor = Color(0xFF403F4C);
 const kFontColorBlue = Color(0xFF207CAF);
 const kFontColorImportant = Color(0xFF207CAF);
 const kBackgroundColors = [Color(0xFFB6E9EB), Color(0xFF207CAF)];
 const kIconColor = Color(0xFF4B4B4B);
 const kBgColorYellow = Color(0xFFFFCD4E);
-// const kBackgroundColorStart = Color(0xFFB6E9EB);
-// const kBackgroundColorEnd = Color(0xFF207CAF);
