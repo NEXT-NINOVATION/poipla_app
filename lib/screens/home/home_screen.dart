e336b0022d8d05a1dbc62f4753a8eb86d556cc50
Ryota-nino <ryota.swimmer@icloud.com>
2022-06-20 10:06:29 +0900

    `add` 冒険画面実装

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -5,7 +5,9 @@ import 'package:poipla_app/constants.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:bordered_text/bordered_text.dart';
 import 'package:poipla_app/screens/home/components/adventure_button.dart';
+import 'package:poipla_app/screens/home/components/adventure_result_modal.dart';
 import 'package:poipla_app/screens/home/components/change_costume_button.dart';
+import 'package:poipla_app/screens/home/components/come_back_timer.dart';
 import 'package:poipla_app/screens/home/components/present_modal.dart';
 import 'package:poipla_app/screens/home/components/qr_modal.dart';
 import 'package:poipla_app/screens/home/components/setting_button.dart';
@@ -26,6 +28,11 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
     // デバイスサイズ
     double deviceW = MediaQuery.of(context).size.width;
     double deviceH = MediaQuery.of(context).size.height;
+
+    // 冒険中フラグ
+    bool isLeave = false;
+    // 帰ってきた表示フラグ
+    bool isExc = false;
 
     final authStore = ref.watch(authStoreProvider);
     return Container(
@@ -74,28 +81,30 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                 height: 200,
                 child: Stack(
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Container(
-                          width: 20,
-                          height: 20,
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.white,
+                    isLeave == true
+                        ? Container()
+                        : Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Container(
+                                width: 20,
+                                height: 20,
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                              Container(
+                                width: 10,
+                                height: 10,
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
-                        const SizedBox(height: 10),
-                        Container(
-                          width: 10,
-                          height: 10,
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
                   ],
                 ),
               ),
@@ -117,35 +126,48 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                             builder: (BuildContext context) => PresentModal(),
                           );
                         },
-                        child: Container(
-                          width: 72,
-                          height: 72,
-                          padding: const EdgeInsets.all(5),
-                          decoration: BoxDecoration(
-                            color: const Color(0xFFFFE6A6),
-                            borderRadius: BorderRadius.circular(4.0),
-                          ),
-                          child: Column(
-                            children: [
-                              Container(
-                                width: 40,
-                                height: 40,
-                                margin: const EdgeInsets.only(bottom: 3.0),
+                        child: Stack(
+                          clipBehavior: Clip.none,
+                          children: [
+                            Container(
+                              width: 72,
+                              height: 72,
+                              padding: const EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                color: const Color(0xFFFFE6A6),
+                                borderRadius: BorderRadius.circular(4.0),
+                              ),
+                              child: Column(
+                                children: [
+                                  Container(
+                                    width: 40,
+                                    height: 40,
+                                    margin: const EdgeInsets.only(bottom: 3.0),
+                                    child: SvgPicture.asset(
+                                      "assets/svg/present.svg",
+                                    ),
+                                  ),
+                                  BorderedText(
+                                    strokeWidth: 2,
+                                    strokeColor: Colors.white,
+                                    child: const Text(
+                                      "プレゼント",
+                                      style: TextStyle(
+                                          color: kFontColorBlue, fontSize: 12),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            if (isExc == true)
+                              Positioned(
+                                right: -12,
+                                top: -12,
                                 child: SvgPicture.asset(
-                                  "assets/svg/present.svg",
+                                  "assets/svg/exc_present.svg",
                                 ),
-                              ),
-                              BorderedText(
-                                strokeWidth: 2,
-                                strokeColor: Colors.white,
-                                child: const Text(
-                                  "プレゼント",
-                                  style: TextStyle(
-                                      color: kFontColorBlue, fontSize: 12),
-                                ),
-                              ),
-                            ],
-                          ),
+                              )
+                          ],
                         ),
                       ),
                       const SizedBox(height: 8.0),
@@ -194,19 +216,137 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                     ],
                   ),
                 ),
-                SvgPicture.asset(
-                  "assets/svg/fish_default.svg",
-                  width: deviceW * 0.6,
-                ),
+                isLeave == true
+                    ? ComeBackTimer()
+                    : Stack(
+                        clipBehavior: Clip.none,
+                        alignment: Alignment.center,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              if (isExc == true) {
+                                showDialog(
+                                  // Dialogの周囲の黒い部分をタップしても閉じないようにする
+                                  barrierDismissible: false,
+                                  context: context,
+                                  builder: (BuildContext context) =>
+                                      const AdventureResultModal(
+                                    prefName: "愛知県",
+                                    kana: "あいちけん",
+                                    earnPoint: 50,
+                                    costumeName: "シャチホコ",
+                                  ),
+                                );
+                              }
+                            },
+                            child: SvgPicture.asset(
+                              "assets/svg/fish_default.svg",
+                              width: deviceW * 0.6,
+                            ),
+                          ),
+                          isExc == true
+                              ? Positioned(
+                                  right: -10,
+                                  top: -120,
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      showDialog(
+                                        // Dialogの周囲の黒い部分をタップしても閉じないようにする
+                                        barrierDismissible: false,
+                                        context: context,
+                                        builder: (BuildContext context) =>
+                                            PresentModal(),
+                                      );
+                                    },
+                                    child: SvgPicture.asset(
+                                      "assets/svg/exc_fish.svg",
+                                    ),
+                                  ),
+                                )
+                              : Container(),
+                        ],
+                      ),
                 Container(
                   margin: const EdgeInsets.only(bottom: 80),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     crossAxisAlignment: CrossAxisAlignment.end,
-                    children: const [
-                      ShopButton(),
-                      AdventureButton(),
-                      ChangeCostumeButton(),
+                    children: [
+                      Stack(
+                        children: [
+                          ShopButton(),
+                          isLeave == true
+                              ? Stack(
+                                  alignment: Alignment.center,
+                                  children: [
+                                    Container(
+                                      width: 90,
+                                      height: 90,
+                                      decoration: const BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: Color(0x88403F4C),
+                                      ),
+                                    ),
+                                    SvgPicture.asset(
+                                      "assets/svg/lock.svg",
+                                      width: 32,
+                                      height: 32,
+                                    )
+                                  ],
+                                )
+                              : Container(),
+                        ],
+                      ),
+                      Stack(
+                        children: [
+                          AdventureButton(),
+                          isLeave == true
+                              ? Stack(
+                                  alignment: Alignment.center,
+                                  children: [
+                                    Container(
+                                      width: 120,
+                                      height: 120,
+                                      decoration: const BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: Color(0x88403F4C),
+                                      ),
+                                    ),
+                                    SvgPicture.asset(
+                                      "assets/svg/lock.svg",
+                                      width: 40,
+                                      height: 40,
+                                    )
+                                  ],
+                                )
+                              : Container(),
+                        ],
+                      ),
+                      Stack(
+                        children: [
+                          ChangeCostumeButton(),
+                          isLeave == true
+                              ? Stack(
+                                  alignment: Alignment.center,
+                                  children: [
+                                    Container(
+                                      width: 90,
+                                      height: 90,
+                                      decoration: const BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: Color(0x88403F4C),
+                                      ),
+                                    ),
+                                    SvgPicture.asset(
+                                      "assets/svg/lock.svg",
+                                      width: 32,
+                                      height: 32,
+                                    )
+                                  ],
+                                )
+                              : Container(),
+                        ],
+                      ),
                     ],
                   ),
                 ),
