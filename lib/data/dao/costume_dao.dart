ca7b59b90540f8222546a7c92323e147d1cde2e7
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-06-28 12:47:01 +0900

    feat: 着せ替えの状態を保存できるようにしました

---
+++ lib/data/dao/costume_dao.dart
@@ -0,0 +1,15 @@
+
+import 'package:poipla_app/data/retrofit/service.dart';
+import 'package:poipla_app/models/entities/costume/costume.dart';
+import 'package:poipla_app/models/repositories/costume_repository.dart';
+
+class CostumeDao implements CostumeRepository {
+  final PoiplaApiService apiService;
+  CostumeDao({required this.apiService});
+
+  @override
+  Future<List<Costume>> getMyCostumes() {
+    return apiService.getMyCostumes();
+  }
+
+}
