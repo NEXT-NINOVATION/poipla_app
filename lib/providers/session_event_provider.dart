5536f1fbdc845372641324413f705b1fa07888c7
Panta <kinoshita062323@gmail.com>
2022-07-01 16:32:45 +0900

    feat: ゴミ箱にゴミが入った時のイベントを取得できるようにしました。

--- lib/providers/session_event_provider.dart
+++ lib/providers/session_event_provider.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:poipla_app/data/events/result_count.dart';
 import 'package:poipla_app/providers/user_provider.dart';
 import 'package:pusher_client/pusher_client.dart';
 
@@ -22,13 +24,17 @@ final pusherClientProvider = Provider.autoDispose<PusherClient>((ref) {
   });
   return client;
 });
-final sessionCountEventStreamProvider =
-    StreamProvider.autoDispose.family((ref, int dustBoxId) async* {
+final sessionCountEventStreamProvider = StreamProvider.autoDispose
+    .family<ResultCount, int>((ref, int dustBoxId) async* {
   final pusherClient = ref.read(pusherClientProvider);
-  final streamController = StreamController<Map<String, dynamic>>();
+  final streamController = StreamController<ResultCount>();
   pusherClient.subscribe('dust-box-$dustBoxId').bind('result-count', (event) {
     log('on session count event, socketId:${pusherClient.getSocketId()}');
     log('result-count-event:${event?.data}');
+    if (event?.data != null) {
+      final json = jsonDecode(event!.data!);
+      streamController.sink.add(ResultCount.fromJson(json));
+    }
     // jsonDecode()
   });
 
