3913fe5cf7b7a7bd100904718c6b67d7f564a3e7
Ryota-nino <nino.brenpy@gmail.com>
2023-02-06 15:03:06 +0900

    `add` ゲームで集めたゴミをホーム画面に表示する欄を追加

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -93,6 +93,76 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
         ),
         body: Stack(
           children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 30),
+                  child: Stack(
+                    clipBehavior: Clip.none,
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        width: 154,
+                        height: 48,
+                        color: const Color(0xFFD2F4FF),
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 5),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(
+                                "あつめたプラのかず",
+                                style: TextStyle(
+                                  color: Color(0xFF207CAF),
+                                  fontSize: 12,
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    "1000000",
+                                    style: TextStyle(
+                                      color: kFontColorRed,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                  Text(
+                                    "こ",
+                                    style: TextStyle(
+                                      color: kFontColorRed,
+                                      fontSize: 12,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: -20,
+                        child: Container(
+                          alignment: Alignment.center,
+                          decoration: const BoxDecoration(
+                            color: Color(0xFF2D9DDC),
+                            shape: BoxShape.circle,
+                          ),
+                          width: 56,
+                          height: 56,
+                          child: SvgPicture.asset(
+                            "assets/svg/score_icon.svg",
+                            width: 40,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
             Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
