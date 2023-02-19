82c4f06c68b03b500c68a46220ba55547ab54059
Ryota-nino <nino.brenpy@gmail.com>
2023-02-20 02:40:10 +0900

    `add` ポイントと集めたプラごみの実装

---
+++ lib/data/dao/game_dao.dart
@@ -0,0 +1,16 @@
+// import 'package:poipla_app/data/retrofit/service.dart';
+// import 'package:poipla_app/models/entities/user/user.dart';
+// import 'package:poipla_app/models/repositories/game_repository.dart';
+
+// class GameDao implements GameRepository {
+//   final PoiplaApiService apiService;
+//   GameDao({required this.apiService});
+
+//   @override
+//   Future<User> postGameResult(
+//       {required int point, required int totalPet}) async {
+//     final res = await apiService
+//         .postGameResult({'point': point, 'total_pet': totalPet});
+//     return res;
+//   }
+// }
